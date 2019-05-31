a72c9a7dfff66fe88724a9400ea0d5062964982b
Liberatys <Ilithium@protonmail.com>
2019-05-31 22:06:29 +0200

    Working on the fix a major bug, list not working as intended

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ On insert of a new item into the vector, rusted-list will lookup the values for 
 Example:
 
 	Current List:
+
 		[3,6,9]
 
 	Stored Elements for value tracking:
@@ -19,6 +20,7 @@ Example:
 		M: 6
 		R: 9
 
+
 	Value to insert:
  		
 		7
@@ -26,8 +28,9 @@ Example:
 	
 	Iterate over stored value ranges and find ranges that encapsulate the value that is to be inserted into the lsit-
 	
-	Left-Cap = 6
-	Right-Cap = 9
+		Left-Cap = 6
+		Right-Cap = 9
+
 
 
 	Iterate over elements between L-Cap && R-Cap and find the position to insert the value to.
@@ -38,8 +41,15 @@ Example:
 		}
 	}
 
+
 	If needed, we will update the stored values for left , middle and right if their possition has been overwritten.
 
+
+	The list will at the moment only track the values for center, left and right. This could/should be changed
+	in a more advanced version of the rusted_list;
+
+	
+		
 
 By keeping track of some "tower" elements in the list, we can insert the value easily without having to iterate over all he value that are in the list. 
 
@@ -48,6 +58,11 @@ By keeping track of some "tower" elements in the list, we can insert the value e
 
 
 After the implementation I will calculate the Big-O notation for the different operations on the list and will compare it to other implementations of an ever lasting sorted list.
+
+
+
+
+
 
 
 
--- src/rusted.rs
+++ src/rusted.rs
@@ -44,42 +44,42 @@ impl RustedList {
             } else if element > self.right_track {
                 self.center_track = self.right_track;
                 self.right_track = element;
-
                 self.value_list.insert(2, element);
             } else {
                 self.center_track = element;
                 self.value_list.insert(1, element);
             }
         } else {
+            self.current_size = self.current_size + 1;
             if element < self.left_track {
                 self.left_track = element;
                 self.value_list.insert(0, element);
+                return;
             } else if element > self.right_track {
                 self.right_track = element;
                 self.value_list.push(element);
-            } else if element > self.center_track {
+                return;
+            }
+            if element > self.center_track {
                 for i in self.center_index..self.current_size - 1 {
-                    let index = i as i32;
-                    if self.value_list[(i as usize)] < element {
+                    if element > self.value_list[(i as usize)] {
                         // if element is bigger than the current index_value, insert the element
                         // one to the right of the current index.
-                        //
-                        self.value_list.insert((index + 1) as usize, element);
+                        self.value_list.insert((i + 1) as usize, element);
                         break;
                     }
                 }
-            } else if element < self.center_track {
+            } else {
                 for i in 0..self.center_index {
                     if self.value_list[(i as usize)] < element {
                         self.value_list.insert((i + 1) as usize, element);
+                        break;
                     }
                 }
-            } else {
-                self.value_list
-                    .insert((self.center_index + 1) as usize, element);
-                self.center_index = self.center_index + 1;
-                self.center_track = element;
             }
+            let center_set_index: i32 = (self.value_list.len() / 2) as i32;
+            println!("{}", center_set_index);
+            self.center_index = center_set_index;
         }
     }
     pub fn remove(&mut self, index: i32) {
@@ -93,8 +93,13 @@ impl RustedList {
     pub fn sorted(&self) -> bool {
         return true;
     }
-}
 
+    pub fn len(&self) -> i32 {
+        return self.current_size;
+    }
+
+    pub fn get_new_insert_position(&self) {}
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -105,5 +110,29 @@ mod tests {
         listing.insert(1);
         listing.insert(5);
         assert_eq!(listing.peek(0), 1);
+    }
+
+    #[test]
+    fn remove_value() {
+        let mut listing = RustedList::new();
+        listing.insert(5);
+        listing.insert(10);
+        listing.remove(1);
+        assert_eq!(listing.peek(0), 5);
+    }
+
+    #[test]
+    fn multiple_insert() {
+        let mut listing = RustedList::new();
+        for i in 0..10 {
+            listing.insert(i);
+        }
+        listing.insert(-1);
+        listing.insert(4);
+        listing.insert(7);
+        for i in 0..listing.len() {
+            println!("{}", listing.peek(i as i32));
+        }
+        assert_eq!(listing.len() as i32, 13);
     }
 }
